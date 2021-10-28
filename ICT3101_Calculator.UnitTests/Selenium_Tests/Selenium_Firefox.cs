cdcb845a023e1fddd3fe1fb5a3eddfe0c65efd66
wala3 <1902665@sit.singaporetech.edu.sg>
2021-10-28 20:38:04 +0800

    lab 5

---
+++ ICT3101_Calculator.UnitTests/Selenium_Tests/Selenium_Firefox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace ICT3101_Calculator.UnitTests.Selenium_Tests
+{
+    [TestFixture]
+    [Parallelizable]
+    public class Selenium_Firefox
+    {
+        private string _testURL = "https://www.google.com";
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void Start_Browser()
+        {
+            // Setup local Selenium WebDriver
+            //_driver = new FirefoxDriver();
+
+            FirefoxOptions option = new FirefoxOptions();
+            option.AddArgument("--headless");
+            _driver = new FirefoxDriver(option);
+        }
+        [Test]
+        public void GoogleSubtract_WhenSubracting2from6_ResultEquals4()
+        {
+            //Setup----------------------------/
+            _driver.Url = _testURL;
+            System.Threading.Thread.Sleep(1000);
+            //Act------------------------------/
+            IWebElement searchBox = _driver.FindElement(By.CssSelector("[name = 'q']"));
+            searchBox.SendKeys("6 - 2" + Keys.Return);
+            System.Threading.Thread.Sleep(1000);
+            //Assert---------------------------/
+            IWebElement calcAnswer = _driver.FindElement(By.Id("cwos"));
+            Assert.That(calcAnswer.Text, Is.EqualTo("4"));
+        }
+        [TearDown]
+        public void Close_Browser()
+        {
+            _driver.Quit();
+        }
+    }
+}
